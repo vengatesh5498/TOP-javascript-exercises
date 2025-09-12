155cb654b857d23e48748f2edfa76085c3847994
Vengatesh Dayalan <155848451+vengatesh5498@users.noreply.github.com>
2025-09-12 07:00:28 +0100

    Loops and Arrays module completed

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year){
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isDivisibleByFourhundred = year % 400 === 0;
 
+    if (isYearDivisibleByFour && (!isCentury || isDivisibleByFourhundred)){
+        return true;
+    }   else {
+        return false;
+    }
 };
 
 // Do not edit below this line
