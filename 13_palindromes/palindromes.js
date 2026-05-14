437de1673f8cec68a962c2ca6a41a975a459c820
Vengatesh Dayalan <155848451+vengatesh5498@users.noreply.github.com>
2026-05-14 20:50:14 +0100

    Palindromes exercise completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+//declaring a constant variable to use as a net to filter within the function call
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    const filteredString = string
+    .toLowerCase()//converts all the alphabets to lower case
+    .split('')//splits the strings to array so array methods can be used on a sting
+    .filter((characters)=>alphanumerical.includes(characters))//filters all the entered characters with the alphanumerical constant variable that's been declared
+    .join();//joins all the array alphabets/numbers to a string
+
+//creating string reversal a.k.a palindrome
+const reversedString = filteredString.split('').reverse().join('');
+return filteredString === reversedString;//returns if the palindrome is true or false
 };
 
 // Do not edit below this line
